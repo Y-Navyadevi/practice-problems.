1acf710400316a04406c6fdec3e6df9fbd5c75d7
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-04-07 19:07:46 +0530

    ex1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app= Flask(__name__)
 
-@app.route('/')
+@app.route('/<name>')
 
-def index():
-    return '<h1>Hello!</h1>'
+def index(name):
+    return '<h1>Hello {}!</h1>'.format(name)
