1924dfffed28ad537f76138463d23450f19e74a4
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-04-07 22:34:25 +0530

    ex

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello WOrld!"
-@app.route("/<string:name>")
-def hello(name):
-    return f"<h1>Hello, {name}!</h1>"
+    return render_template("index.html")
+
+
+#@app.route("/<string:name>")
+##ef hello(name):
+   # return f"<h1>Hello, {name}!</h1>"
+
+
+
 #@app.route("/david")
 #def david():
   #  return "Hello David!"
