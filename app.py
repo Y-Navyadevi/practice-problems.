47fd3fa6451fabac2ca9db25eb0976ef6c954294
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-04-07 19:20:54 +0530

    commit

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ app= Flask(__name__)
 @app.route("/")
 def index():
     return "Hello WOrld!"
-@app.route("/david")
-def david():
-    return "Hello David!"
+@app.route("/<string:name>")
+def hello(name):
+    return f"Hello, {name}!"
+#@app.route("/david")
+#def david():
+  #  return "Hello David!"
 
-@app.route("/maria")
-def david():
-    return "Hello DMaria!"
+#@app.route("/maria")
+#def david():
+#    return "Hello DMaria!"
 
 
 #@app.route('/<name>')
